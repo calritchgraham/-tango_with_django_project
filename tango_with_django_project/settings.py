b65e6f36a91aa4094c3e659ee4819b1b145dad2b
“Callum <2225081g@student.gla.ac.uk>
2021-07-26 19:09:53 +0100

    4.3 complete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -66,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -124,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path 
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 """tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -25,4 +27,4 @@ path('', views.index, name='index'),
 path('rango/', include('rango.urls')),
 # The above maps any URLs starting with rango/ to be handled by rango. 
 path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
