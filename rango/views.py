dd85bfd9902e4c9f4026eb4f25056f1d8dc70c86
“Callum <2225081g@student.gla.ac.uk>
2021-07-30 23:13:01 +0100

    Ch 10 test passed

--- rango/views.py
+++ rango/views.py
@@ -26,8 +26,7 @@ def index(request):
     context_dict['pages'] = page_list
     
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-      
+          
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
@@ -35,15 +34,11 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Callum Graham'}
     
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     
     return render(request, 'rango/about.html', context=context_dict)
-    
-    
-    # return HttpResponse("Rango says here is the about page. \
-    #     <br/> <a href='/rango/'>index</a>")
+
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
